afd037f3f78306850263e026e203e0f744c85c29
accolombini <accolombini@gmail.com>
2021-03-14 21:41:02 -0300

    Criado repositório Analise de Dados com Pandas

---
+++ APRENDIZADO_MAQUINA/Python/ANALISE_DADOS_PANDAS/Ola.py
@@ -0,0 +1,7 @@
+# Testando o ambiente
+
+print(f"\nPandas para análise de dados...")
+print(f"\nInicio dos trabalhos com Pandas!!")
+print(f"\nInstalando bibliotecas: pip intall pandas")
+print(f"\nInstalando bibliotecas: pip intall matplotilib")
+print(f"\nInstalando bibliotecas: pip install seaborn")
